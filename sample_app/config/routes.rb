84176077197b0735d3d3cd1c7d230ca763caef90
Restia1201 <yoshiki1201.1995@gmail.com>
2022-04-02 17:26:34 +0000

    [Update]４章終了

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   get 'lists/new'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  get 'lists/:id' =>'lists#show', as: 'list'
   get 'lists/edit'
 
 end
