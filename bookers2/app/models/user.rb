1208741a0fc860708355b9abac0e5cda51de680e
Restia1201 <yoshiki1201.1995@gmail.com>
2022-05-08 11:35:16 +0000

    [Update]15章 Viewから

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -6,5 +6,14 @@ class User < ApplicationRecord
   has_one_attached :image
   has_many :books, dependent: :destroy
 
+  has_one_attached :profile_image
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      profile_image.attached(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
