1218106608c3029559fbf9dc8b6befdb4bc23c53
Restia1201 <yoshiki1201.1995@gmail.com>
2022-06-05 11:35:03 +0000

    [Update]グリッドシステム導入

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { in: 2..20 }
   validates :introduction, length: {maximum: 50}
-  
+
   def get_profile_image(width,height)
    unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
