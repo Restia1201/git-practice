9f412bc396a824a02c9d5ecbd375bfea5c45d3f9
Restia1201 <yoshiki1201.1995@gmail.com>
2022-06-12 10:58:48 +0000

    [Update]投稿・削除・更新機能が働いていない

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,15 +1,14 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-     redirect_to book_path(@book)
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
     else
-     render :new
+     @books = Book.all
+     render :index
     end
   end
 
@@ -29,12 +28,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully"
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
   end
 
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.permit(:book).permit(:title, :body, :image)
   end
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy ]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:index, :show, :edit, :update]
-
   get 'homes/about' => 'homes#about', as:'about'
 end
